0fd6a6e3379c1c1af2c5cc11f9853721a0920844
Mohit <51738793+Mohit0033@users.noreply.github.com>
2021-05-01 22:11:05 +0530

    Update config.go

--- build/soong/android/config.go
+++ build/soong/android/config.go
@@ -1,13 +1,13 @@
 package android
 
 // Global config used by Evolution X soong additions
-var EvolutionConfig = struct {
+var hyperxConfig = struct {
 	// List of packages that are permitted
 	// for java source overlays.
 	JavaSourceOverlayModuleWhitelist []string
 }{
 	// JavaSourceOverlayModuleWhitelist
 	[]string{
-		"org.evolution.hardware",
+		"org.hyperx.hardware",
 	},
 }
