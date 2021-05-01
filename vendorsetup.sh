ed27c70aaec2087c53d28df9b4370348b0bfaf8c
Mohit <51738793+Mohit0033@users.noreply.github.com>
2021-05-01 22:04:32 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,6 @@ lunch_others_targets=()
 for device in $(python vendor/evolution/tools/get_official_devices.py)
 do
     for var in user userdebug eng; do
-        lunch_others_targets+=("evolution_$device-$var")
+        lunch_others_targets+=("hyperx_$device-$var")
     done
 done
